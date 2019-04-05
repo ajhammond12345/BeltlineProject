31defdfd0771da780a220836aaa51d385eb83a84
Alexander Hammond <ajhammond123@gmail.com>
2019-04-05 12:08:56 -0400

    Initial Commit

---
+++ DatabasesProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DatabasesProject
+//
+//  Created by Alexander Hammond on 4/5/19.
+//  Copyright © 2019 Team24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
